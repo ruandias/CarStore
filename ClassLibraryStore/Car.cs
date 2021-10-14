e16036b646cc46edc2c7e24d3e90dcd090963d8a
avanade.ruan <avanade.ruan@grupofleury.com.br>
2021-10-14 11:09:45 -0300

    created a classlib to share classes to other projects

---
+++ ClassLibraryStore/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassLibraryStore
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+            Make = "Nothing yet";
+            Model = "Nothing yet";
+            Price = 0.00M;
+        }
+
+        public Car(string make, string model, decimal price)
+        {
+            Make = make;
+            Model = model;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Make} {Model} R${Price}";
+        }
+    }
+}
---
+++ ClassLibraryStore/Store.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibraryStore
+{
+    public class Store
+    {
+        public List<Car> CarList { get; set; }
+        public List<Car> ShoppingList { get; set; }
+
+        public Store()
+        {
+            CarList = new List<Car>();
+            ShoppingList = new List<Car>();
+        }
+
+        public decimal CheckOut()
+        {
+            decimal totalCost = 0;
+
+            foreach(var c in ShoppingList)
+            {
+                totalCost += c.Price;
+            }
+
+            ShoppingList.Clear();
+
+            return totalCost;
+        }
+    }
+}
